920c55b0b11c035412148fd0b6447edbc9fd8911
Cheyne Hayworth <degenftm@gmail.com>
2022-03-20 10:40:53 +1100

    add voltage

--- src/static/js/fuse.js
+++ src/static/js/fuse.js
@@ -8,11 +8,12 @@ const main = async() => {
     "title":"Fuse Network",
     "heading":["Pool Provider","LP", "Reward Tokens", "INFO"],
     "rows": [
-      ["Fuse                      ",`<a href="fuseio"                 >Various</a>`,"FUSE               ","https://fuse.io"],
+      ["Fuse *                    ",`<a href="fuseio"                 >Various</a>`,"FUSE               ","https://fuse.io"],
       ["Elk                       ",`<a href="elk"                    >Various</a>`,"ELK                ","https://app.elk.finance"],
       ["Beefy                     ",`<a href="beefy"                  >Various</a>`,"BIFI               ","https://app.beefy.finance/#/fuse"],
       ["Sushi                     ",`<a href="sushi"                  >Various</a>`,"FUSE               ","https://app.sushi.com"],
-      ["Nextgen                   ",`<a href="nextgen"                >Various</a>`,"NGEN               ","https://app.next-gen.finance"]
+      ["Nextgen                   ",`<a href="nextgen"                >Various</a>`,"NGEN               ","https://app.next-gen.finance"],
+      ["Voltage                   ",`<a href="voltage"                >Various</a>`,"VOLT               ","https://app.voltage.com"],
     ]
   }
 
